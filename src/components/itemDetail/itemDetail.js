9acda4530e0f4b6eb9948f9a3a4bbf6b5d2d4ce7
Alan Deniz <alandeniz95@hotmail.com>
2021-05-06 18:05:37 -0300

    dando estilo

--- src/components/itemDetail/itemDetail.js
+++ src/components/itemDetail/itemDetail.js
@@ -2,14 +2,11 @@ import { ItemCount } from "../itemCount/itemCount";
 export const ItemDetail = (props) => {
   //console.log(props.tite);
   return (
-    <div className="items">
+    <div className="itemDetail">
       <h4>{props.title}</h4>
       <ul>
         <li>
           <strong>Precio $ {props.price}</strong>
-        </li>
-        <li>
-          <strong>Comentarios: {props.comment}</strong>
         </li>
       </ul>
       <ItemCount />
